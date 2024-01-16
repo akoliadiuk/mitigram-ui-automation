7c5d2395deb914c0f6c2eb577409789168ffccc2
akoli252 <39258964+akoli252@users.noreply.github.com>
2024-01-17 00:31:48 +0200

    Added email validation test

---
+++ test/data/errors.js
@@ -0,0 +1,6 @@
+export const LOGIN_VALIDATION_ERRORS = {
+  EMAIL_REQUIRED: 'Email is required The Email field is not a valid e-mail address.',
+  EMAIL_INVALID: 'The Email field is not a valid e-mail address.',
+  PASSWORD_REQUIRED: 'Password is required',
+  INVALID_LOGIN_ATTEMPT: 'Invalid login attempt.',
+};
--- test/specs/login.spec.js
+++ test/specs/login.spec.js
@@ -2,12 +2,22 @@
 /* eslint-disable playwright/no-skipped-test */
 import { expect } from '@playwright/test';
 import { test } from '../../pages/fixtures';
+import { LOGIN_VALIDATION_ERRORS } from '../data/errors';
+import fs from 'fs';
+import path from 'path';
+import { parse } from 'csv-parse/sync';
 
-test.describe.configure({ mode: 'serial' });
+const ivalidEmails = parse(fs.readFileSync(path.join(__dirname, '../data/invalid-emails.csv')),
+  { columns: true, relax_quotes: true });
 
 const PAGE_TITLE = 'Mitigram';
 
 let loginPage;
+
+const verifyErrorMessage = async (errorMessage) => {
+  await expect(loginPage.validationError).toBeInViewport();
+  await expect(loginPage.validationError).toHaveText(errorMessage);
+};
 
 test.beforeAll('Open Login page', async ({ loginPageFixture }) => {
   loginPage = await loginPageFixture.open();
@@ -16,7 +26,6 @@ test.beforeAll('Open Login page', async ({ loginPageFixture }) => {
 test.afterAll('Close Login page', async () => {
   await loginPage.close();
 });
-
 
 test('has title @critical', async () => {
   await expect(loginPage.page).toHaveTitle(PAGE_TITLE);
@@ -40,59 +49,48 @@ test('has login form inputs ready to use @critical', async () => {
 
 test('requires email @critical', async () => {
   await loginPage.loginButton.click();
-  await expect(loginPage.validationError).toBeVisible();
-  await expect(loginPage.validationError)
-      .toHaveText('Email is required The Email field is not a valid e-mail address.');
+  await verifyErrorMessage(LOGIN_VALIDATION_ERRORS.EMAIL_REQUIRED);
 });
 
 test('requires password @critical', async ({ user }) => {
   await loginPage.emailField.fill(user.email);
   await loginPage.loginButton.click();
-  await expect(loginPage.validationError).toBeVisible();
-  await expect(loginPage.validationError).toHaveText('Password is required');
+  await verifyErrorMessage(LOGIN_VALIDATION_ERRORS.PASSWORD_REQUIRED);
 });
 
 test('validates credentials @critical', async ({ user }) => {
   await loginPage.emailField.fill(user.email);
   await loginPage.passwordField.fill(user.password);
   await loginPage.loginButton.click();
-  await expect(loginPage.validationError).toHaveText('Invalid login attempt.');
+  await verifyErrorMessage(LOGIN_VALIDATION_ERRORS.INVALID_LOGIN_ATTEMPT);
 });
 
-test.skip('successfully logs in @critical', async ({ user }) => {
+test.fixme('successfully logs in @critical', async ({ user }) => {
   await loginPage.emailField.fill(user.email);
   await loginPage.passwordField.fill(user.password);
   await loginPage.loginButton.click();
   // await homePage.waitForLoaded();
   // await expect(homePage.page).toHaveURL(homePage.url);
 });
-test.fixme('has app store badges visible and clickable @high', async () => { });
-test.fixme('has footer "Solutions" links visible and clickable @high', async () => { });
-test.fixme('has footer "Platform" links visible and clickable @high', async () => { });
-test.fixme('has footer "Company" links visible and clickable @high', async () => { });
-test.fixme('has footer social media links visible and clickable @high', async () => { });
 
-test('scrolls to open positions once "Open Positions" button clicked @high', async () => {
-  await careersPage.openPositionsAnchor.click();
-  await expect(careersPage.openPositionsFilters).toBeInViewport();
-  // TODO mock the response to have at least one position for sure
-  await expect(careersPage.openPositionsItem.first()).toBeInViewport();
+test('requires email to be valid: @high', async () => {
+  for (const email of ivalidEmails) {
+    await test.step(`${email.description}`, async () => {
+      await loginPage.emailField.fill(email.value);
+      await loginPage.loginButton.click();
+      await verifyErrorMessage(LOGIN_VALIDATION_ERRORS.EMAIL_INVALID);
+    });
+  }
 });
 
-test.fixme('allows filtering positions by location @high', async () => { });
-test.fixme('preserves filter on page navigation @high', async () => { });
-test.fixme('has all the links navigate to correct pages @high', async () => { });
+test.fixme('has app store badges visible and clickable @high', async () => { });
 
-test.fixme('displays empty state list if no positions matches filter @medium', async () => { });
-test.fixme('displays empty state list when no positions found @medium', async () => { });
-
-test.fixme('has all the buttons on the page clickable @medium', async () => { });
 test.fixme('displays other UI elements @medium', async () => { });
 
 test.fixme('has correct hover state for buttons and links @medium', async () => { });
 
 test.fixme('handles window resizing so that all UI elements are reachable @medium',
-    async () => { });
+  async () => { });
 
 test.fixme('handles different error states when API call fails @medium', async () => { });
 
